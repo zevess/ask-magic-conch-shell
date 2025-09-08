8373fde77a6f30516c231aa617ace3dc3e9b1714
zevess <gazetdinovvlad04@gmail.com>
2025-09-08 17:32:48 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
+'use client'
+import { FormEvent, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const audioRef = useRef<HTMLAudioElement>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [text, setText] = useState("")
+
+
+  const questionAnswer = () => {
+    const yes = "yes"
+    const no = "no"
+    const randomIndex = Math.floor(Math.random() * 2)
+    const result = randomIndex === 0 ? yes : no
+    return result
+  }
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const answer = questionAnswer()
+    playAnswer(`../${answer}.m4a`)
+    setText("")
+  }
+
+  const playAnswer = (answer: string) => {
+    const audioElement = audioRef.current
+    if (audioElement instanceof HTMLAudioElement) {
+      audioElement.src = answer
+      audioElement.play().then(() => {
+        setIsPlaying(true)
+      })
+    }
+
+    if (videoRef.current) {
+      videoRef.current.play()
+    }
+  }
+
+
+
+  return (
+    <div className="flex justify-center">
+      <video ref={videoRef} onClick={(e) => e.currentTarget.play()} muted className="h-screen w-full object-fill">
+        <source src="../shell43.mp4" />
+        <audio ref={audioRef} onEnded={() => setIsPlaying(false)} />
+      </video>
+
+      {!isPlaying && <div className={`absolute w-full h-full flex flex-col justify-center items-center`}>
+        <form onSubmit={onSubmit} className="max-w-2/3 flex flex-col justify-center items-center bg-white/30 backdrop-blur-xs rounded-2xl p-5 gap-6 animate-[fadeIn_0.8s_ease-in-out_1]">
+          <h1 className="text-5xl">Спроси у Волшебной ракушки</h1>
+          <input type="text" placeholder="Ваш вопрос..." value={text} onChange={(e) => setText(e.target.value)} className="w-full bg-white backdrop-blur-xs border-2 rounded-lg h-12 text-2xl" />
+        </form>
+      </div>
+      }
     </div>
   );
 }
