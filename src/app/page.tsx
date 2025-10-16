ed7307f695be9cb6adb2a0696d1358a1d3f0be0d
zevess <gazetdinovvlad04@gmail.com>
2025-10-16 23:12:20 +0300

    text-black in dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { FormEvent, useRef, useState } from "react";
-
 export default function Home() {
 
   const audioRef = useRef<HTMLAudioElement>(null)
@@ -39,7 +38,6 @@ export default function Home() {
   }
 
 
-
   return (
     <div className="flex justify-center">
       <video ref={videoRef} preload="auto" onClick={(e) => e.currentTarget.play()} muted className="h-screen w-full object-fill">
@@ -49,8 +47,8 @@ export default function Home() {
 
       {!isPlaying && <div className={`absolute w-full h-full flex flex-col justify-center items-center`}>
         <form onSubmit={onSubmit} className="max-w-2/3 flex flex-col justify-center items-center bg-white/30 backdrop-blur-xs rounded-2xl p-5 gap-6 animate-[fadeIn_0.8s_ease-in-out_1]">
-          <h1 className="text-5xl">Спроси у Волшебной ракушки</h1>
-          <input type="text" placeholder="Ваш вопрос..." value={text} onChange={(e) => setText(e.target.value)} className="w-full bg-white backdrop-blur-xs border-2 rounded-lg h-12 text-2xl" />
+          <h1 className="text-5xl dark:text-black">Спроси у Волшебной ракушки</h1>
+          <input type="text" placeholder="Ваш вопрос..." value={text} onChange={(e) => setText(e.target.value)} className="w-full bg-white backdrop-blur-xs border-2 rounded-lg h-12 text-2xl dark:text-black" />
         </form>
       </div>
       }
